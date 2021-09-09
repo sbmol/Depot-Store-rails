f81ac2deb855d2098929e442a1b470fc11ee3c0c
sbmol <sebastianmolinasb@gmail.com>
2021-09-09 18:09:10 -0300

    update react pay_type

---
+++ app/javascript/PayTypeSelector/NoPayType.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class NoPayType extends React.Component {
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+export default NoPayType
